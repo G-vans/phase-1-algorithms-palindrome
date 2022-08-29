4a2166c75b8f90596f7c4ee14ce66d0f3ad6d9e0
G-vans <jevans.otieno@student.moringaschool.com>
2022-08-29 05:30:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 function isPalindrome(word) {
   // Write your algorithm here
+
+  let rightIdx = 0;
+let leftIdx = word.length - 1;
+
+while (rightIdx < leftIdx) {
+  if(word[rightIdx] !== word[leftIdx]) {
+    return false;
+  }
+
+  rightIdx++;
+  leftIdx--;
 }
+return true;
+
+}
+
+// function pali(word){
+//   return word.split("").reverse().join("")
+  
+
+// }
+
+
+// console.log(pali("abba"));
+
+
 
 /* 
   Add your pseudocode here
@@ -13,13 +38,29 @@ function isPalindrome(word) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  console.log("Expecting: true")
+  console.log("=>", isPalindrome("abba"))
+
+  console.log("")
+
+  console.log("Expecting: true")
+  console.log("=>", isPalindrome("racecar"))
+
+  console.log("")
+
+  console.log("Expecting: true")
+  console.log("=>",isPalindrome("a"))
+
+  console.log("")
+
+
   console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+  console.log("=>", isPalindrome("robot"));
 
   console.log("");
 
   console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+  console.log("=>", isPalindrome("ab"));
 }
 
 module.exports = isPalindrome;
